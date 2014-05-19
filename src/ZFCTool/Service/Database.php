b3243fe2027b5104b73f12d7122ebabd22176880
Alexander Khaylo <alexander.khaylo@nixsolutions.com>
2014-05-19 16:52:01 +0300

    Added disable checks notation

--- src/ZFCTool/Service/Database.php
+++ src/ZFCTool/Service/Database.php
@@ -122,9 +122,26 @@ class Database
             }
         }
 
+        if ($dump) {
+            $dump = $this->getDisableChecksNotation() . $dump;
+        }
+
         return $dump;
     }
 
+    /**
+     * @return string
+     */
+    public function getDisableChecksNotation()
+    {
+        $sql = "/*!40101 SET NAMES utf8 */;\n"
+            . "/*!40101 SET SQL_MODE=''*/;\n"
+            . "/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;\n"
+            . "/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;\n"
+            . "/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;\n"
+            . "/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;\n\n";
+        return $sql;
+    }
 
     /**
      * retrieve index list from table
